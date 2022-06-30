3c62615d57678f25efe4a8a469d0ba9243345b90
sharonmayberg <sharonm@tailorbrands.com>
2022-06-30 16:51:23 +0300

    added zendesk addon enabled

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: require('./package').name,
 
   contentFor: function (type, config) {
-    if (type === 'head' && config.zendesk && config.zendesk.key) {
+    if (type === 'head' && config.zendesk && config.zendesk.enabled && config.zendesk.key) {
       const srcPath = "https://static.zdassets.com/ekr/snippet.js?key=" + config.zendesk.key;
       return `<script id="ze-snippet" src=${srcPath}></script>`;
     }
